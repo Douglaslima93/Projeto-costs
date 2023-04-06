d3191c4cc0d909e21d20cd4e3472646ef37199d9
Douglas Lima <douglas07a@gmail.com>
2023-04-06 13:54:05 -0300

    Criação de novos componentes e estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
+import './main.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-     
-    </div>
+    <Container customClass='min-height'>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo projeto</Link>
+      </div>
+      <Routes>
+        
+      <Route exact path='/' element={<Home />}>
+        </Route>
+
+        <Route exact path='/company' element={<Company />}>
+        </Route>
+
+        <Route exact path='/contact' element={<Contact />}>
+        
+        </Route>
+
+        <Route exact path='/newproject' element={<NewProject />}>
+        </Route>
+        
+      </Routes>
+      <p>Footer</p>
+    </Router>
+    </Container>
   )
 }
 
